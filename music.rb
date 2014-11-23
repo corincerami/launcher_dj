143d173cf10ce839aba14d195f6d3e27cd0afa56
Chris C Cerami <chrisccerami@gmail.com>
2014-11-23 14:21:54 -0500

    Query built to add songs to database

--- music.rb
+++ music.rb
@@ -18,6 +18,11 @@ artist = "Dexys Midnight Runner"
 user_name = "Chris Magic Wand"
 url = "https://www.youtube.com/watch?v=oc-P8oDuS0Q"
 
+# user to be able to post these above variables in a form
+# pull those from params and insert into db query
+# build another query to select all songs from table
+# deliver those songs to .erb file
+
 query = "INSERT INTO songs (song_name, artist, user_name, song_url, created_at)
     VALUES ('#{song_name}', '#{artist}', '#{user_name}', '#{url}', now());"
 
