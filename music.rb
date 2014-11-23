e63534e5f125639af39bf5211eae3a9650283f7a
Chris C Cerami <chrisccerami@gmail.com>
2014-11-23 14:20:36 -0500

    Query built to add songs to database

--- music.rb
+++ music.rb
@@ -1,11 +1,10 @@
-require "sinatra"
 require "pg"
 require "pry"
 
 
 def db_connection
   begin
-    connection = PG.connect(dbname: 'launch_playlist')
+    connection = PG.connect(dbname: 'launcher_dj')
 
     yield(connection)
 
@@ -14,3 +13,15 @@ def db_connection
   end
 end
 
+song_name = "Come On, Eileen"
+artist = "Dexys Midnight Runner"
+user_name = "Chris Magic Wand"
+url = "https://www.youtube.com/watch?v=oc-P8oDuS0Q"
+
+query = "INSERT INTO songs (song_name, artist, user_name, song_url, created_at)
+    VALUES ('#{song_name}', '#{artist}', '#{user_name}', '#{url}', now());"
+
+# add song to the database
+db_connection do |conn|
+    conn.exec(query)
+end
